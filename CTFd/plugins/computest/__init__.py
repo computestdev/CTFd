70563ac2e38738fd87d64a4b15092f92878bd302
Serrano Pereira <spereira@computest.nl>
2017-12-01 16:30:50 +0100

    Add challenge type that sends email notifications (CTC-44)
    An email notification is sent on each challenge solve or fail.

--- CTFd/plugins/computest/__init__.py
+++ CTFd/plugins/computest/__init__.py
@@ -1,4 +1,7 @@
+import logging
 import os
+import smtplib
+import sys
 
 from flask import (
     render_template, render_template_string, redirect, url_for, request,
@@ -7,10 +10,166 @@ from sqlalchemy.sql.expression import union_all
 from sqlalchemy import distinct
 
 from CTFd import utils
-from CTFd.models import db, Teams, Solves, Awards, Challenges
+from CTFd.models import db, Teams, Solves, Awards, Challenges, Keys, WrongKeys
+from CTFd.plugins import challenges, register_plugin_assets_directory
 
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+MAIL_TEMPLATE_SOLVED = """Hello,
+
+User {team.name} (id: {team.id}) just solved challenge {challenge.name} of category {challenge.category} with key '{flag}'.
+
+Regards,
+Computest Challenges
+"""
+MAIL_TEMPLATE_FAILED = """Hello,
+
+User {team.name} (id: {team.id}) just failed challenge {challenge.name} of category {challenge.category} with key '{flag}'.
+
+Regards,
+Computest Challenges
+"""
+STDERR_FORMAT = "ctfd[%(process)d]: %(name)s %(levelname)s: %(message)s"
+
+logger = logging.getLogger('ctfd.plugins.computest')
+logger.setLevel(logging.NOTSET)
+
+stderr_handler = logging.StreamHandler(sys.stderr)
+stderr_handler.setFormatter(logging.Formatter(STDERR_FORMAT))
+stderr_handler.setLevel(logging.INFO)
+
+logger.addHandler(stderr_handler)
+
+# challenge types
+
+class NotifyingChallenge(challenges.CTFdStandardChallenge):
+
+    """Challenge type that sends notifications for solves/fails.
+
+    The :meth:`solve` and :meth:`fail` methods are overwritten to send email
+    notifications. The :meth:`create` method is only overwritten so that the
+    :class:`NotifyingChallenges` model is used to store challenges.
+    """
+
+    id = "notifying"
+    name = "notifying"
+    templates = {
+        'create': '/plugins/computest/assets/notifying-challenge-create.hbs',
+        'update': '/plugins/challenges/assets/standard-challenge-update.hbs',
+        'modal': '/plugins/challenges/assets/standard-challenge-modal.hbs',
+    }
+    scripts = {
+        'create': '/plugins/challenges/assets/standard-challenge-create.js',
+        'update': '/plugins/challenges/assets/standard-challenge-update.js',
+        'modal': '/plugins/challenges/assets/standard-challenge-modal.js',
+    }
+
+    @staticmethod
+    def send_email(text):
+        to_address = utils.get_config('challenge_notification_address')
+        if to_address is None:
+            logger.error(
+                "failed to send email notification because "
+                "challenge_notification_address is not set")
+            return
+
+        sent = False
+        try:
+            sent = utils.sendmail(to_address, text)
+
+        except smtplib.SMTPException:
+            logger.exception(
+                "an exception occurred while trying to send email notification")
+
+        if not sent:
+            logger.warning("failed to send email notification")
+
+    @classmethod
+    def send_notification(cls, solve):
+        if not isinstance(solve, (WrongKeys, Solves)):
+            raise TypeError("expected a Solves or WrongKeys instance")
+
+        challenge = Challenges.query.get(solve.chalid)
+        team = Teams.query.get(solve.teamid)
+
+        if isinstance(solve, WrongKeys):
+            cls.send_email(MAIL_TEMPLATE_FAILED.format(
+                flag=solve.flag, challenge=challenge, team=team))
+        elif isinstance(solve, Solves):
+            cls.send_email(MAIL_TEMPLATE_SOLVED.format(
+                flag=solve.flag, challenge=challenge, team=team))
+
+    @staticmethod
+    def create(request):
+        files = request.files.getlist('files[]')
+
+        # Create challenge
+        chal = NotifyingChallenges(
+            name=request.form['name'],
+            description=request.form['desc'],
+            value=request.form['value'],
+            category=request.form['category'],
+            type=request.form['chaltype']
+        )
+
+        if 'hidden' in request.form:
+            chal.hidden = True
+        else:
+            chal.hidden = False
+
+        max_attempts = request.form.get('max_attempts')
+        if max_attempts and max_attempts.isdigit():
+            chal.max_attempts = int(max_attempts)
+
+        db.session.add(chal)
+        db.session.commit()
+
+        flag = Keys(chal.id, request.form['key'], request.form['key_type[0]'])
+        if request.form.get('keydata'):
+            flag.data = request.form.get('keydata')
+        db.session.add(flag)
+
+        db.session.commit()
+
+        for f in files:
+            utils.upload_file(file=f, chalid=chal.id)
+
+        db.session.commit()
+
+    @classmethod
+    def solve(cls, team, chal, request):
+        provided_key = request.form['key'].strip()
+        solve = Solves(teamid=team.id, chalid=chal.id,
+                       ip=utils.get_ip(req=request), flag=provided_key)
+        db.session.add(solve)
+        cls.send_notification(solve)
+        db.session.commit()
+        db.session.close()
+
+    @classmethod
+    def fail(cls, team, chal, request):
+        provided_key = request.form['key'].strip()
+        wrong = WrongKeys(teamid=team.id, chalid=chal.id,
+                          ip=utils.get_ip(request), flag=provided_key)
+        db.session.add(wrong)
+        cls.send_notification(wrong)
+        db.session.commit()
+        db.session.close()
+
+
+# models
+
+
+class NotifyingChallenges(Challenges):
+
+    """Define the Challenge model for NotifyingChallenge.
+
+    This is required for NotifyingChallenge to work.
+    """
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'notifying'
+    }
 
 
 class TeamScoreVisibility(db.Model):
@@ -93,7 +252,7 @@ def get_standings(category=None, count=None):
         results.columns.teamid
     ).subquery()
 
-    # Filters out banned users.
+    # Filters out banned and hidden users.
     # Properly resolves value ties by ID.
     #
     # Different databases treat time precision differently so resolve by the
@@ -171,7 +330,7 @@ def scoreboard_by_category(app):
 
 
 def load(app):
-    # Creat tables.
+    # Create plugin tables.
     app.db.create_all()
 
     # Disable teams list.
@@ -180,6 +339,11 @@ def load(app):
     # Display scores per category on scoreboard.
     scoreboard_by_category(app)
 
+    # Register the notifying challenge type.
+    register_plugin_assets_directory(app, base_path='/plugins/computest/assets/')
+    challenges.CHALLENGE_CLASSES["notifying"] = NotifyingChallenge
+
+    # Add custom routes.
     @app.route('/profile/preferences', methods=['GET'])
     def profile_preferences():
         """View for displaying profile preferences."""
@@ -203,7 +367,9 @@ def load(app):
         template = os.path.join(DIR_PATH, 'templates/preferences.html')
 
         return render_template_string(
-            open(template).read(), nonce=session.get('nonce'), visible=visible,
+            open(template).read(),
+            nonce=session.get('nonce'),
+            visible=visible,
             success=False)
 
     @app.route('/profile/preferences', methods=['POST'])
@@ -229,5 +395,44 @@ def load(app):
         template = os.path.join(DIR_PATH, 'templates/preferences.html')
 
         return render_template_string(
-            open(template).read(), nonce=session.get('nonce'), visible=visible,
+            open(template).read(),
+            nonce=session.get('nonce'),
+            visible=visible,
+            success=True)
+
+    @app.route('/admin/computest', methods=['GET'])
+    @utils.admins_only
+    def admin_computest():
+        """View for displaying admin preferences for the Computest plugin."""
+        if not session.get('nonce'):
+            session['nonce'] = utils.sha512(os.urandom(10))
+
+        challenge_notification_address = utils.get_config(
+            'challenge_notification_address') or ''
+
+        template = os.path.join(DIR_PATH, 'templates/admin_computest.html')
+
+        return render_template_string(
+            open(template).read(),
+            nonce=session.get('nonce'),
+            challenge_notification_address=challenge_notification_address,
+            success=False)
+
+    @app.route('/admin/computest', methods=['POST'])
+    @utils.admins_only
+    def admin_computest_submit():
+        """View for submitting admin preferences for the Computest plugin."""
+        utils.set_config(
+            'challenge_notification_address',
+            request.form.get('challenge_notification_address', None))
+
+        challenge_notification_address = utils.get_config(
+            'challenge_notification_address') or ''
+
+        template = os.path.join(DIR_PATH, 'templates/admin_computest.html')
+
+        return render_template_string(
+            open(template).read(),
+            nonce=session.get('nonce'),
+            challenge_notification_address=challenge_notification_address,
             success=True)
